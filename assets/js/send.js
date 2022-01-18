6eac6826083fec88ac7e0264da6a79bca8bc48b8
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2022-01-18 17:06:23 +0300

    Buton Düzeltildi.

--- assets/js/send.js
+++ assets/js/send.js
@@ -12,28 +12,29 @@ const nodemailer = require('nodemailer')
 //       return false;
 // }}
 
-let transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:'elsyazilimsendmail@gmail.com',
-        pass: 'cnzcfleblonnvgos'
+
+
+function sendMail() {
+
+    let transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'elsyazilimsendmail@gmail.com',
+            pass: 'cnzcfleblonnvgos'
+        }
+    })
+    
+    let mailOptions = {
+        from: 'elsyazilimsendmail@gmail.com',
+        to: 'oguzhanagir1@gmail.com',
+        subject: 'ELS Yazılım Web Sitesi İletişim Formu',
+        text: 'deneme bir maildir els den gönderildi'
+    
     }
-})
 
-let mailOptions = {
-    from: 'elsyazilimsendmail@gmail.com',
-    to: 'oguzhanagir1@gmail.com',
-    subject: 'ELS Yazılım Web Sitesi İletişim Formu',
-    text: 'deneme bir maildir site içeriği'
-
-}
-
-var btn = document.getElementById('btn_contact');
-
-btn.onclick = function(){
     transporter.sendMail(mailOptions,(err,data) =>{
         if(err) console.log(err)
         else console.log('Mail Başarılı Bir Şekilde Gönderildi.')
     })
-    
 }
+
