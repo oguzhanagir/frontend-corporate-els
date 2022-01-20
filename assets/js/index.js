83fd6be79cfc64d6470cd9fbbfceaf561cd2cf73
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2022-01-20 17:27:44 +0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,7 @@
-// const nodemailer = require('nodemailer')
-
-
-// function saveInfo(e) {
-//     const name = document.querySelector("#inputName").value;
-//     const email = document.querySelector("#inputEmail").value;
-//     const phone = document.querySelector("#inputPhone").value;
-//     const message = document.querySelector("#inputMessage").value;
-  
-//     if (!name && !email && !phone && !message) {
-//       alert("Lütfen Formu Doldurunuz");
-//       return false;
-// }}
-
-// let transporter = nodemailer.createTransport({
-//     service:'gmail',
-//     auth:{
-//         user:'elsyazilimsendmail@gmail.com',
-//         pass: 'cnzcfleblonnvgos'
-//     }
-// })
-
-// let mailOptions = {
-//     from: 'elsyazilimsendmail@gmail.com',
-//     to: 'oguzhanagir1@gmail.com',
-//     subject: 'ELS Yazılım Web Sitesi İletişim Formu',
-//     text: 'deneme bir maildir els den gönderildi'
-
-// }
-
-// function sendMail() {
-//     transporter.sendMail(mailOptions,(err,data) =>{
-//         if(err) console.log(err)
-//         else console.log('Mail Başarılı Bir Şekilde Gönderildi.')
-//     })
-// }
-
 function smtpMail() {
 	Email.send({
 	Host: "smtp.gmail.com",
-	Username : "elsyazilim",
+	Username : "elsyazilimsendmail@gmail.com",
 	Password : "cnzcfleblonnvgos",
 	To : 'oguzhanagir1@gmail.com',
 	From : "elsyazilimsendmail@gmail.com",
