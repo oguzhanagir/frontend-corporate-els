d4cc0c9322e69c105d5b61566bf95e73420137b9
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2022-01-20 17:25:19 +0300

    smtp

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,3 +35,16 @@ function sendMail() {
     })
 }
 
+function smtpMail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "elsyazilim",
+	Password : "cnzcfleblonnvgos",
+	To : 'oguzhanagir1@gmail.com',
+	From : "elsyazilimsendmail@gmail.com",
+	Subject : "Smtp den gönderildi",
+	Body : "els mesaj içerği",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
